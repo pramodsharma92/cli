7405a25c1951a870da267575ec6121bf8a4f0502
pramodsharma92 <pramod.a.sharma46@gmail.com>
2017-10-24 18:30:49 +0530

    Update create_vm.sh

--- create_vm.sh
+++ create_vm.sh
@@ -1,1 +1,1 @@
-aws ec2 run-instances --image-id ami-9fa343e7 --count 1 --instance-type t2.micro --key-name aws --security-group-ids sg-f480f689 --subnet-id subnet-323fbc7a --associate-public-ip-address --query 'Instances[0].InstanceId' --output text
+~/.local/bin/aws ec2 run-instances --image-id ami-9fa343e7 --count 1 --instance-type t2.micro --key-name aws --security-group-ids sg-f480f689 --subnet-id subnet-323fbc7a --associate-public-ip-address --query 'Instances[0].InstanceId' --output text
